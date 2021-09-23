c39d2bb9ebd297faf07b917a1b244218392d19fe
CodeHz <codehz@outlook.com>
2021-09-23 18:45:17 +0800

    Fix types

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,24 @@
-import { Readable } from 'stream';
-import { Api } from 'telegram';
+import { Api } from "telegram";
+export * from "tgcalls/lib/types";
 
 export interface JoinParams {
-    joinAs?: Api.TypeEntityLike;
-    muted?: boolean;
-    videoStopped?: boolean;
-    inviteHash?: string;
+  joinAs?: Api.TypeEntityLike;
+  muted?: boolean;
+  videoStopped?: boolean;
+  inviteHash?: string;
 }
 
 export interface MediaParams {}
 
 export interface EditParams {
-    muted?: boolean;
-    volume?: number;
-    raiseHand?: boolean;
-    videoStopped?: boolean;
-    videoPaused?: boolean;
-    presentationPaused?: boolean;
+  muted?: boolean;
+  volume?: number;
+  raiseHand?: boolean;
+  videoStopped?: boolean;
+  videoPaused?: boolean;
+  presentationPaused?: boolean;
 }
 
 export interface Listeners {
-    [K: `on${string}`]: Function | undefined;
-}
-
-export interface Media<T> {
-    readable?: Readable;
-    options?: T
-}
-
-export interface Video {
-    width?: number;
-    height?: number;
-    framerate?: number;
-}
-
-export interface Audio {
-    bitsPerSample?: number;
-    sampleRate?: number;
-    channelCount?: number;
-    almostFinishedTrigger?: number;
+  [K: `on${string}`]: Function | undefined;
 }
